c9e93d8632d98da2ddfde7904e3e1b2932457aa9
maliy <tubusovsergej@gmail.com>
2022-11-02 15:08:59 +0200

    css final

---
+++ JS/sctipt.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 0,
+        nav: true,
+        navText: ['', ' '],
+        autoplayTimeout: 2500,
+        dots: true,
+
+        responsive: {
+            0: {
+                items: 1,
+                autoplay: true
+            },
+            830: {
+                items: 2,
+                autoplay: true
+            },
+
+            1000: {
+                items: 3
+            }
+        }
+    })
+});
